5b91efc626db44007ed0b29172b71dbad9c6bba4
adiyadav255 <adiyadav255@gmail.com>
2025-10-30 00:37:42 +0530

    Update calibration routines for gas sensors
    Updated MQ135 and MQ7 sensor initialization to use MQUnifiedsensorLIB, adjusted calibration routines

--- main/node_v2_csv/node_v2_csv.ino
+++ main/node_v2_csv/node_v2_csv.ino
@@ -1,24 +1,22 @@
 #include<PMS.h>
 #include<DHT.h>
-#include<MQ135.h>
 #include<MQUnifiedsensor.h>
 #define DHTTYPE DHT11
 #define DHTPIN 4
 #define RXD2 16
 #define TXD2 17
 #define MQ135_PIN 36
-#define RZERO_135 0.00 //IN CLEAN AIR
 #define MQ7_PIN 39     
 #define BOARD "ESP-32" 
 #define VOLT_RES 3.3  
 #define ADC_BIT_RES 12 
-#define RL 10.0       
-#define RZERO_7 10.00 //IN CLEAN AIR
-#define GAS "CO"
+#define RL 1.0
+#define RZERO_135 60.00
+#define RZERO_7 18.00       
 //DHT, MQ135, PMS//
 DHT dht(DHTPIN, DHTTYPE);
-MQ135 co2sensor(MQ135_PIN, RZERO_135);
-MQUnifiedsensor coSensor(BOARD, VOLT_RES, ADC_BIT_RES, MQ7_PIN, GAS);
+MQUnifiedsensor coSensor(BOARD, VOLT_RES, ADC_BIT_RES, MQ7_PIN);
+MQUnifiedsensor co2Sensor(BOARD, VOLT_RES, ADC_BIT_RES, MQ135_PIN);
 PMS pms(Serial2);
 PMS::DATA data;
 void setup() {
@@ -34,19 +32,36 @@ void setup() {
   coSensor.setRL(RL);
   coSensor.setA(99.042); 
   coSensor.setB(-1.518);
-  // MQ-7 Calibration routine 
+  coSensor.setRegressionMethod(1);  
+  // MQ-7 Calibration routine, optional after first run
   Serial.print("Calibrating MQ7 please wait.");
   float calcR0 = 0;
   for(int i = 1; i<=10; i ++) {
     coSensor.update();
-    calcR0 += coSensor.calibrate(27.5); 
-    Serial.print(".");
+    calcR0 += coSensor.calibrate(22.00);
+    delay(500);
   }
   coSensor.setR0(calcR0/10);
-  Serial.println(" done!.");
+  Serial.print("R0_MQ7 = "); Serial.println(calcR0 / 10);
   coSensor.serialDebug(true);
+  // --- MQ135 Initialization ---
+  co2Sensor.init();
+  co2Sensor.setR0(RZERO_135);
+  co2Sensor.setRL(RL);
+  co2Sensor.setRegressionMethod(1);
+  co2Sensor.setA(110.47); // Empirical constants for CO2 from MQ135 datasheet
+  co2Sensor.setB(-2.862);
+  Serial.print("Calibrating MQ135... ");
+  float calcR0_CO2 = 0;
+  for (int i = 0; i < 10; i++) {
+    co2Sensor.update();
+    calcR0_CO2 += co2Sensor.calibrate(0.6);
+    delay(500);
+  }
+  co2Sensor.setR0(calcR0_CO2 / 10);
+  Serial.print("R0_MQ135 = "); Serial.println(calcR0_CO2 / 10);
   Serial.println("---------------------------------");
-  Serial.println("Time(ms),Temp(°C),Humidity(%),PM2.5,PM10,CO2/VOC(ppm)");
+  Serial.println("Time(ms),Temp(°C),Humidity(%),PM2.5,PM10,CO2/VOC(ppm),CO(ppm)");
 }
 void loop() {
   //PMS//
@@ -54,17 +69,22 @@ void loop() {
   //DHT11//
   float t=dht.readTemperature();
   float rh=dht.readHumidity();
-  //MQ135//
-  float ppm1=co2sensor.getPPM();
   //MQ7//
   coSensor.update(); 
-  float ppm2=coSensor.readSensor(false, 0.0);
+  float ppm1=co2Sensor.readSensor();
+  //MQ135//
+  co2Sensor.update();
+  float ppm2=coSensor.readSensor();
   //csv output//
   Serial.print(millis()/1000.0); Serial.print(",");
   Serial.print(t); Serial.print(",");
   Serial.print(rh); Serial.print(",");
   Serial.print(data.PM_AE_UG_2_5); Serial.print(",");
   Serial.print(data.PM_AE_UG_10_0); Serial.print(",");
+  if(isnan(ppm1))
+  {
+    ppm1=0.00;
+  }
   if(isnan(ppm2))
   {
     ppm2=0.00;
--- mq135_calibration/mq135_calibration.ino
+++ mq135_calibration/mq135_calibration.ino
@@ -3,9 +3,12 @@
 MQ135 mq135_sensor(MQ135_PIN);
 void setup() {
   Serial.begin(115200);
+  Serial.println("Power ON");
+  Serial.println("-----------------------");
+}
+void loop() {
   float rzero = mq135_sensor.getRZero();
   Serial.print("Value: ");
   Serial.println(rzero);
-}
-void loop() {
+  delay(3000);
 }
