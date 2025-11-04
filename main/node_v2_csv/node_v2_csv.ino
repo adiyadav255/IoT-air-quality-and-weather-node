d08b7d03b991ad442627885d116ef9f0e6110eb3
adiyadav255 <adiyadav255@gmail.com>
2025-11-04 21:24:30 +0530

    Adjusted sensor calibration and renamed files
    Renamed data_logger_v2.py to data_logger_UDP.py and data_logger_v1.py to data_logger_serial.py for clarity. Added new outdoor AQI CSV data and updated node_v2_csv.ino to send UDP Packets

--- main/node_v2_csv/node_v2_csv.ino
+++ main/node_v2_csv/node_v2_csv.ino
@@ -1,6 +1,8 @@
 #include<PMS.h>
 #include<DHT.h>
 #include<MQUnifiedsensor.h>
+#include<WiFi.h>
+#include<WiFiUdp.h>
 #define DHTTYPE DHT11
 #define DHTPIN 4
 #define RXD2 16
@@ -19,6 +21,11 @@ MQUnifiedsensor coSensor(BOARD, VOLT_RES, ADC_BIT_RES, MQ7_PIN);
 MQUnifiedsensor co2Sensor(BOARD, VOLT_RES, ADC_BIT_RES, MQ135_PIN);
 PMS pms(Serial2);
 PMS::DATA data;
+char ssid[] = "Connected, no internet";
+char pass[] = "Aditya@12345";
+const char* pc_ip="10.96.51.48";
+const int pc_port=9000;
+WiFiUDP udp;
 void setup() {
   Serial.begin(115200);
   Serial.println("Power ON");
@@ -26,6 +33,7 @@ void setup() {
   Serial.println("DHT init");
   Serial2.begin(9600,SERIAL_8N1, RXD2, TXD2);
   Serial.println("PMS init");
+  WiFi.begin(ssid,pass);
   // --- MQ-7 Initialization ---
   coSensor.init(); 
   coSensor.setR0(RZERO_7);
@@ -38,7 +46,7 @@ void setup() {
   float calcR0 = 0;
   for(int i = 1; i<=10; i ++) {
     coSensor.update();
-    calcR0 += coSensor.calibrate(22.00);
+    calcR0 += coSensor.calibrate(22.8);
     delay(500);
   }
   coSensor.setR0(calcR0/10);
@@ -55,7 +63,7 @@ void setup() {
   float calcR0_CO2 = 0;
   for (int i = 0; i < 10; i++) {
     co2Sensor.update();
-    calcR0_CO2 += co2Sensor.calibrate(0.6);
+    calcR0_CO2 += co2Sensor.calibrate(5.5);
     delay(500);
   }
   co2Sensor.setR0(calcR0_CO2 / 10);
@@ -80,7 +88,13 @@ void loop() {
   Serial.print(t); Serial.print(",");
   Serial.print(rh); Serial.print(",");
   Serial.print(data.PM_AE_UG_2_5); Serial.print(",");
-  Serial.print(data.PM_AE_UG_10_0); Serial.print(",");
+  Serial.print(data.PM_AE_UG_10_0); Serial.println(",");
+  //UDP Packet//
+  char buffer[128];
+  snprintf(buffer, sizeof(buffer), "%.2f,%.2f,%.2f,%d,%d,%.2f,%.2f\n",millis()/1000.0, t, rh, data.PM_AE_UG_2_5, data.PM_AE_UG_10_0, ppm1, ppm2);
+  udp.beginPacket(pc_ip, pc_port);
+  udp.print(buffer);
+  udp.endPacket();
   if(isnan(ppm1))
   {
     ppm1=0.00;
@@ -91,6 +105,6 @@ void loop() {
   }
   Serial.print(ppm1,2); Serial.print(",");
   Serial.println(ppm2,2);
-  delay(5000);
+  delay(15000);
   }
 }
